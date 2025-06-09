797d1250f6f1b018c6ad16f6a6caaeae8109ce45
root <root@Mims-LAPTOP.localdomain>
2025-06-09 21:17:17 +0200

    python00

--- python_0_starting/ex08/Loading.py
+++ python_0_starting/ex08/Loading.py
@@ -1,12 +1,26 @@
-from tqdm import tqdm
+
+import time
 from time import sleep
+import os
+
+def progressBar(barlen: int, curr: int, total_items: int) -> str:
+     
 
 def ft_tqdm(lst: range) -> None:
-    for i in range(101):
-        yield print(f'{i}%')
+    total_items = len(lst)
+    size = os.get_terminal_size()
+    reqChar = 8 + (len(str(total_items )) * 2) + 26
+    barlen = size.columns - reqChar
+    starttime = time.time()
+
+    for curr in lst:
+        display = str()
+        display = progressBar(barlen, curr, total_items)
+        display = timesstamp(starttime, curr, total_items)
+        print(display, end='\r')
+        yield
+    
+    return
 
 for i in ft_tqdm(range(333)):
-    sleep(0.005)
-
-for i in tqdm(range(1000)):
     sleep(0.005)
