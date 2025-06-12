74057d806e2e57f8031ba743dec9792d599a65ae
Mehdi Achouba <machouba@f2r2s4.clusters.42paris.fr>
2025-06-12 19:30:04 +0200

    Python01

---
+++ Python_1_Array/ex03/load_image.py
@@ -0,0 +1,17 @@
+from PIL import Image
+import matplotlib.pyplot as plt
+import numpy as np
+
+def ft_load(path: str) -> list:
+    try:
+        #shape not 3 et si tu te trompe de format
+        if path[-5:] != ".jpeg" and path[-4:] != ".jpg":
+            raise FileNotFoundError("bad format .jpeg or .jpg")
+        img = np.asanyarray(Image.open(path))
+        print("The shape of image is: ", img.shape)
+        return img
+    except Exception as Error:
+        print("An error occurred:", Error)
+        return []
+
+print(ft_load("animal.jpeg"))
